270fedbbaa09f229ed8ad6e4e84ebc692e83dd0e
Your Name <you@example.com>
2024-11-06 14:06:38 +0530

    deployed

--- ticket-backend-master/ticket-backend-master/app.js
+++ ticket-backend-master/ticket-backend-master/app.js
@@ -1,49 +1,50 @@
-import dotenv from "dotenv"
-import express from "express"
-import cors from "cors"
-import cookieParser from "cookie-parser"
-import helmet from "helmet"
-import morgan from "morgan"
+import dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import helmet from "helmet";
+import morgan from "morgan";
 
-import userRouter from "./routes/userRoutes.js"
-import dashboardRouter from "./routes/dashboardRoutes.js"
-import companyRouter from "./routes/companyRoutes.js"
-import ticketRouter from "./routes/ticketRoutes.js"
-import errorMiddleware from "./middlewares/Error.js"
+import userRouter from "./routes/userRoutes.js";
+import dashboardRouter from "./routes/dashboardRoutes.js";
+import companyRouter from "./routes/companyRoutes.js";
+import ticketRouter from "./routes/ticketRoutes.js";
+import errorMiddleware from "./middlewares/Error.js";
 
-export const app = express()
+export const app = express();
 
 dotenv.config();
 
-//inbuilt middlewares
+// Inbuilt middlewares
 app.use(helmet());
-
 app.use(morgan("combined"));
 
 app.use(
-  cors()
+  cors({
+    origin: ["https://ticketing-b3da9.web.app", "http://localhost:5173"], // Allow requests from specific origins
+    methods: ["GET", "POST", "PUT", "DELETE", "UPDATE"],
+    credentials: true, // Allow cookies and other credentials
+  })
 );
 
-app.use(cookieParser())
 
+app.use(cookieParser());
 app.use(express.json());
-
 app.use(
-    express.urlencoded({
-        extended: true,
-    })
+  express.urlencoded({
+    extended: true,
+  })
 );
 
 app.get("/", (req, res, next) => {
-    res.send("server is working");
-})
+  res.send("server is working");
+});
 
-//routes
+// Routes
 app.use("/api", userRouter);
 app.use("/api/company", companyRouter);
 app.use("/api/ticket", ticketRouter);
 app.use("/api/dashboard", dashboardRouter);
 
-
-//custom error handler
-app.use(errorMiddleware)
+// Custom error handler
+app.use(errorMiddleware);
--- ticket-backend-master/ticket-backend-master/server.js
+++ ticket-backend-master/ticket-backend-master/server.js
@@ -1,21 +1,28 @@
 import { app } from "./app.js";
 import { connectDb } from "./configurations/connection.js";
-import cloudinary from "cloudinary"
+import cloudinary from "cloudinary";
 import dotenv from "dotenv";
 import cors from "cors";
 
+dotenv.config(); // Load environment variables
 
 app.use(
-  cors()
+  cors({
+    origin: ["https://ticketing-b3da9.web.app", "http://localhost:5173"], // Allow requests from specific origins
+    methods: ["GET", "POST", "PUT", "DELETE", "UPDATE"],
+    credentials: true, // Allow cookies and other credentials
+  })
 );
-connectDb()
+
+
+connectDb();
 
 cloudinary.v2.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET_KEY,
-  });
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET_KEY,
+});
 
 app.listen(process.env.PORT, () => {
-    console.log(`server is listening to port : ${process.env.PORT}`)
-})
+  console.log(`Server is listening on port: ${process.env.PORT}`);
+});
